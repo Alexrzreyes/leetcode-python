3d1a98d580a83bbb045b7390c3c44316ff117ddd
Alexisrzreyes <alexis.rz.reyes@gmail.com>
2026-04-28 16:12:27 -0600

    feat: leetcode-53-maxsubarray

---
+++ 53-maxsubarray.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane's Algorithm
+        max_so_far = nums[0]
+        current_max = nums[0]
+        
+        for i in range(1, len(nums)):
+            current_max = max(nums[i], current_max + nums[i])
+            max_so_far = max(max_so_far, current_max)
+            
+        return max_so_far
